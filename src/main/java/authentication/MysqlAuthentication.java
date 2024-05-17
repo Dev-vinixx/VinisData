d218513d687a6862e5d6eecf63bd08542aaf3758
Dev-vinixx <viniciuseduardo0500@gmail.com>
2024-05-17 12:36:43 -0300

    feat(authentication): add method to retrieve JDBC Driver
    - Implemented the method to get the JDBC Driver class for MySQL connection

--- src/main/java/authentication/MysqlAuthentication.java
+++ src/main/java/authentication/MysqlAuthentication.java
@@ -4,11 +4,14 @@ import org.jetbrains.annotations.*;
 
 import java.net.InetAddress;
 import java.sql.Connection;
+import java.sql.Driver;
+import java.util.concurrent.CompletableFuture;
 
 public class MysqlAuthentication {
 
     private final @Nullable String username;
     private final @Nullable String password;
+
 
     private final @NotNull InetAddress hostname;
 
@@ -54,5 +57,32 @@ public class MysqlAuthentication {
         return port;
     }
 
+    public final @NotNull Class<Driver> getDriver() {
 
+        try {
+            try {
+                return (Class<Driver>) Class.forName("com.mysql.cj.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                return (Class<Driver>) Class.forName("com.mysql.jdbc.Driver");
+            }
+        } catch (Throwable e) {
+            throw new RuntimeException("Cannot load default driver 'com.mysql.jdbc.Driver'", e);
+        }
+    }
+
+
+    public final @NotNull CompletableFuture<Connection> connect() {
+        if (isConnected()) {
+            throw new IllegalStateException("This authentication already are connected!");
+        }
+
+        @NotNull CompletableFuture<Connection> future = new CompletableFuture<>();
+
+        CompletableFuture.runAsync(() -> {
+            try {
+                Class<Driver> driver = getDriver();
+
+            }
+        });
+    }
 }
