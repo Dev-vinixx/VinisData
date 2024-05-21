7e839bd33fcb2ea089d488909f25ed56bd6d506d
Dev-vinixx <viniciuseduardo0500@gmail.com>
2024-05-21 06:45:56 -0300

    feat: Add asynchronous read method for database queries
    This method allows reading data from a specified table in a database. It accepts parameters for the database, table, columns, and conditions. The results are returned as a CompletableFuture containing a list of maps, where each map represents a data row with column names and corresponding values.

--- src/main/java/codes/vinis/crud/Crud.java
+++ src/main/java/codes/vinis/crud/Crud.java
@@ -7,13 +7,14 @@ import codes.vinis.database.Database;
 import org.jetbrains.annotations.*;
 
 import java.net.InetAddress;
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
-import java.sql.PreparedStatement;
 
 public class Crud {
 
@@ -37,10 +38,13 @@ public class Crud {
         @NotNull CompletableFuture<Void> future = new CompletableFuture<>();
 
         CompletableFuture.runAsync(() -> {
-            try (@NotNull Connection connection = mysqlAuthentication.connect().join()) {
+            try {
+                @Nullable Connection connection;
 
-                if (connection == null) {
-                    throw new SQLException("connection is null");
+                if (mysqlAuthentication.getConnection() != null) {
+                    connection = mysqlAuthentication.getConnection();
+                } else {
+                    connection = mysqlAuthentication.connect().join();
                 }
 
                 String columns = values.keySet().stream().map(Column::toString).collect(Collectors.joining(", "));
@@ -66,6 +70,68 @@ public class Crud {
     }
 
     /**
+     * Asynchronously performs a query to read data from a database table.
+     *
+     * @param database   The database to query.
+     * @param table      The table from which data will be read.
+     * @param columns    The specific columns to select in the query.
+     * @param condition  The conditions to filter the results (column and values).
+     * @return A CompletableFuture containing a list of maps, where each map represents a data row.
+     *         Each map's key is the column name, and the value is the corresponding value.
+     * @throws SQLException if an error occurs during query execution.
+     */
+    @NotNull
+    public final CompletableFuture<List<Map<String, Object>>> read(@NotNull Database database, @NotNull Table table, @NotNull List<Column> columns, @NotNull Map<Column, List<@Nullable Object>> condition) {
+        @NotNull CompletableFuture<List<Map<String, Object>>> future = new CompletableFuture<>();
+
+        CompletableFuture.runAsync(() -> {
+            try {
+                @Nullable Connection connection;
+
+                if (mysqlAuthentication.getConnection() != null) {
+                    connection = mysqlAuthentication.getConnection();
+                } else {
+                    connection = mysqlAuthentication.connect().join();
+                }
+
+                @NotNull String setCause = columns.stream().map(Column::toString).collect(Collectors.joining(", "));
+                @NotNull String setConditions = condition.entrySet().stream()
+                        .map(entry -> entry.getKey() + " IN (" + entry.getValue().stream().map(v -> "?").collect(Collectors.joining(", ")) + ")")
+                        .collect(Collectors.joining(" AND "));
+                @NotNull String sql = "SELECT " + setCause + " FROM " + database.toString() + "." + table.toString() + " WHERE " + setConditions;
+
+                try (@NotNull PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                    int index = 1;
+                    for (List<Object> values : condition.values()) {
+                        for (Object value : values) {
+                            preparedStatement.setObject(index++, value);
+                        }
+                    }
+
+                    try (@NotNull ResultSet resultSet = preparedStatement.executeQuery()) {
+                        ResultSetMetaData metaData = resultSet.getMetaData();
+                        int columnCount = metaData.getColumnCount();
+
+                        List<Map<String, Object>> results = new ArrayList<>();
+                        while (resultSet.next()) {
+                            Map<String, Object> row = new HashMap<>();
+                            for (int i = 1; i <= columnCount; i++) {
+                                row.put(metaData.getColumnName(i), resultSet.getObject(i));
+                            }
+                            results.add(row);
+                        }
+                        future.complete(results);
+                    }
+                }
+            } catch (@NotNull Throwable throwable) {
+                future.completeExceptionally(throwable);
+            }
+        });
+
+        return future;
+    }
+
+    /**
      * Asynchronous method to update a record in a specific table.
      *
      * @param database  The database containing the table.
@@ -80,10 +146,13 @@ public class Crud {
         @NotNull CompletableFuture<Void> future = new CompletableFuture<>();
 
         CompletableFuture.runAsync(() -> {
-            try (@NotNull Connection connection = mysqlAuthentication.connect().join()) {
+            try {
+                @Nullable Connection connection;
 
-                if (connection == null) {
-                    throw new SQLException("connection is null");
+                if (mysqlAuthentication.getConnection() != null) {
+                    connection = mysqlAuthentication.getConnection();
+                } else {
+                    connection = mysqlAuthentication.connect().join();
                 }
 
                 String setClause = values.keySet().stream()
@@ -133,10 +202,13 @@ public class Crud {
         @NotNull CompletableFuture<Void> future = new CompletableFuture<>();
 
         CompletableFuture.runAsync(() -> {
-            try (@NotNull Connection connection = mysqlAuthentication.connect().join()) {
+            try {
+                @Nullable Connection connection;
 
-                if (connection == null) {
-                    throw new SQLException("connection is null");
+                if (mysqlAuthentication.getConnection() != null) {
+                    connection = mysqlAuthentication.getConnection();
+                } else {
+                    connection = mysqlAuthentication.connect().join();
                 }
 
                 String whereClause = condition.keySet().stream()
